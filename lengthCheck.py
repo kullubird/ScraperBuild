3b6fa8f3464e6c57b9cf6c2cf6c081970c200158
kullubird <kevindsouza@live.com>
2018-09-18 01:13:18 +0530

    length check makes sure no unique values

--- lengthCheck.py
+++ lengthCheck.py
@@ -6,18 +6,47 @@ apiKeyInstance = ApiKey()
 myRegion = apiKeyInstance.myRegion
 capsRegion = apiKeyInstance.capsRegion
 
-    
-listNewMatchIds=open("Scraped Data/"+str(myRegion)+"/newMatchIds"+str(capsRegion)+".txt")
-listDoneUserIds=open("Scraped Data/"+str(myRegion)+"/doneUserIds"+str(capsRegion)+".txt")
-
+listNewMatchIds=open('Scraped Data/'+myRegion+'/newMatchIds'+capsRegion+'.txt').read().splitlines()
+listDoneMatchIds=open('Scraped Data/'+myRegion+'/doneMatchIds'+capsRegion+'.txt').read().splitlines()
+listNewUserIds=open('Scraped Data/'+myRegion+'/newUserIds'+capsRegion+'.txt').read().splitlines()
+listDoneUserIds=open('Scraped Data/'+myRegion+'/doneUserIds'+capsRegion+'.txt').read().splitlines()
 
 #converting lists into sets so no all unique values
 setNewMatchIds=set(listNewMatchIds)
+setDoneMatchIds=set(listDoneMatchIds)
+setNewUserIds=set(listNewUserIds)
 setDoneUserIds=set(listDoneUserIds)
+
+
 
 print("Region : "+capsRegion)
 print("setNewMatchIds= "+str(len(setNewMatchIds)))
 print("setDoneUserIds= "+str(len(setDoneUserIds)))
+
+	#refill the files when program is halted
+filePointer=open("Scraped Data/"+myRegion+"/newUserIds"+capsRegion+".txt","w")
+for lines in setNewUserIds:
+	temp=str(lines)
+	filePointer.write("%s\n"%temp)
+filePointer.close()
+
+filePointer=open("Scraped Data/"+myRegion+"/doneUserIds"+capsRegion+".txt","w")
+for lines in setDoneUserIds:
+	temp=str(lines)
+	filePointer.write("%s\n"%temp)
+filePointer.close()
+
+filePointer=open("Scraped Data/"+myRegion+"/newMatchIds"+capsRegion+".txt","w")
+for lines in setNewMatchIds:
+	temp=str(lines)
+	filePointer.write("%s\n"%temp)
+filePointer.close()
+
+filePointer=open("Scraped Data/"+myRegion+"/doneMatchIds"+capsRegion+".txt","w")
+for lines in setDoneMatchIds:
+	temp=str(lines)
+	filePointer.write("%s\n"%temp)
+filePointer.close()
 
 
 ##have to do more for euw1
